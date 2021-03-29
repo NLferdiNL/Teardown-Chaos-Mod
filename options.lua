9ac70df179fff50d46afb9e18767930fc77cfb2e
Ferdi Alleman <ferdialleman@gmail.com>
2021-03-29 16:51:25 +0200

    Reworked options menu
    Huge thanks to superfroggman, modulo operations are truly something.

--- options.lua
+++ options.lua
@@ -20,34 +20,34 @@ textboxClass = {
 		lastInputActive = false,
 		
 		render = (function(me)
-		UiPop()
-		
-		UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
-		UiTranslate(-me.width - #me.name * 2.7, 0)
-		UiText(me.name .. ":")
-		UiTranslate(me.width + #me.name * 2.7, 0)
-		
-		if me.checkMouseInRect(me) and not me.inputActive then
-			UiColor(1,1,0)
-		elseif me.inputActive then
-			UiColor(0,1,0)
-		else
-			UiColor(1,1,1)
-		end
-		
-		local tempVal = me.value
-		
-		if tempVal == "" then
-			tempVal = " "
-		end
-		
-		if UiTextButton(tempVal, me.width, me.height) then
-			me.inputActive = not me.inputActive
-		end
-		
-		UiColor(1,1,1)
-		
 		UiPush()
+			
+			UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
+			UiTranslate(-me.width - #me.name * 2.7, 0)
+			UiText(me.name .. ":")
+			UiTranslate(me.width + #me.name * 2.7, 0)
+			
+			if me.checkMouseInRect(me) and not me.inputActive then
+				UiColor(1,1,0)
+			elseif me.inputActive then
+				UiColor(0,1,0)
+			else
+				UiColor(1,1,1)
+			end
+			
+			local tempVal = me.value
+			
+			if tempVal == "" then
+				tempVal = " "
+			end
+			
+			if UiTextButton(tempVal, me.width, me.height) then
+				me.inputActive = not me.inputActive
+			end
+			
+			UiColor(1,1,1)
+			
+		UiPop()
 		end),
 		
 		checkMouseInRect = (function(me)
@@ -110,65 +110,60 @@ textboxClass = {
 
 function draw()
 	local textBox01, newBox01 = getTextBox(1)
-
+	
 	--[[local mX, mY = UiGetMousePos()
 	UiButtonImageBox("ui/common/box-solid-6.png", 6, 6)
 	UiTranslate(mX, mY)
 	UiRect(10, 10)
 	UiTranslate(-mX, -mY)]]--
-
-	UiWordWrap(400)
 	
-	UiTranslate(UiCenter(), 50)
-	UiAlign("center middle")
-	
-	UiFont("bold.ttf", 48)
-	UiTranslate(0, 50)
-	UiText("Chaos Mod")
-	
-	UiFont("regular.ttf", 26)
 	UiPush()
+		UiTranslate(UiWidth(), UiHeight())
+		UiTranslate(-50, 3 * -50)
+		UiAlign("right bottom")
 	
-	UiTranslate(0, 50)
-	
-	UiText("To backspace an input box press Ctrl.")
-	
-	UiTranslate(0, 50)
-	
-	UiText("A better options screen is being made!")
-	
-	UiTranslate(0, -50)
+		UiFont("regular.ttf", 26)
 		
-	UiTranslate(400, UiHeight() - 300)
-	
-	UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
-	
-	if UiTextButton("Reset to default", 200, 50) then
-		textBox01.value = 10 .. ""
-		chaosEffects.disabledEffects = {}
-	end
-	
-	UiTranslate(0, 60)
-	
-	if UiTextButton("Save and exit", 200, 50) then
-		local saveData = SerializeTable(chaosEffects.disabledEffects)
-		SetString(moddataPrefix .. "DisabledEffects", saveData)
-		SetInt(moddataPrefix .. "ChaosTimer", tonumber(textBox01.value))
-		Menu()
-	end
-	
-	UiTranslate(0, 60)
-	
-	if UiTextButton("Cancel", 200, 50) then
-		Menu()
-	end
-	
-	UiTranslate(-400, -UiHeight() + 360)
-	
-	UiTranslate(0, 50)
-	
+		UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
+		
+		if UiTextButton("Reset to default", 200, 50) then
+			textBox01.value = 10 .. ""
+			chaosEffects.disabledEffects = {}
+		end
+		
+		UiTranslate(0, 60)
+		
+		if UiTextButton("Save and exit", 200, 50) then
+			local saveData = SerializeTable(chaosEffects.disabledEffects)
+			SetString(moddataPrefix .. "DisabledEffects", saveData)
+			SetInt(moddataPrefix .. "ChaosTimer", tonumber(textBox01.value))
+			Menu()
+		end
+		
+		UiTranslate(0, 60)
+		
+		if UiTextButton("Cancel", 200, 50) then
+			Menu()
+		end
 	UiPop()
-		UiTranslate(0, 150)
+	
+	UiPush()
+		UiWordWrap(400)
+	
+		UiTranslate(UiCenter(), 50)
+		UiAlign("center middle")
+	
+		UiFont("bold.ttf", 48)
+		UiTranslate(0, 50)
+		UiText("Chaos Mod")
+	
+		UiFont("regular.ttf", 26)
+	
+		UiTranslate(0, 50)
+		
+		UiText("To backspace an input box press Ctrl.")
+	
+		UiTranslate(0, 50)
 		
 		if newBox01 then
 			textBox01.name = "Chaos timer"
@@ -184,48 +179,67 @@ function draw()
 		UiTranslate(0, 35)
 		
 		UiText("How long between each effect?")
-	UiPush()
-	
 	UiPop()
-		UiTranslate(0, 50)
+	
+	UiPush()
+		UiWordWrap(400)
+	
+		UiTranslate(UiCenter(), 300)
+		UiAlign("center middle")
+		UiFont("regular.ttf", 26)
+		
 		UiText("Enabled effects:")
-		UiTranslate(0, 30)
+	UiPop()
+	
+	UiPush()
+		UiTranslate(UiCenter(), 350)
+		UiAlign("center middle")
+		UiFont("regular.ttf", 26)
 		
-		local rows = 5
-		local col = 4
+		local rows = 4
+		--local col = 4
 		
-		local xOffset = 300 * rows / 2
-		local margin = 10
+		-- local xOffset = 300 * rows / 2.7
+		-- local xMargin = 20
+		-- local yMargin
+		
+		local buttonWidth = 320
+		local buttonHeight = 40
 		
 		local items = chaosEffects.effectKeys
 		
+		UiTranslate(-buttonWidth * 1.5, 0)
+		
 		for key, value in ipairs(items) do
-			--DebugPrint(key .. ": " .. -xOffset + (key % rows) * 300 .. ", " .. key / col * 40)
-			
-			--UiTranslate(0, (key - 1) * 40 + 5)
-			
-			UiTranslate(-xOffset + margin + (key % rows) * 300, key / col * 40)
-			
-			local effectDisabled = chaosEffects.disabledEffects[value] ~= nil
-			
-			if effectDisabled then
-				UiButtonImageBox("ui/common/box-outline-6.png", 6, 6, 1, 0, 0, 1)
-			else
-				UiButtonImageBox("ui/common/box-outline-6.png", 6, 6, 0, 1, 0, 1)
-			end
-			
-			if UiTextButton(chaosEffects.effects[value].name, 300, 40) then
+			--UiPush()
+				
+				--UiTranslate(-xOffset + (key % rows) * buttonWidth, math.floor((key / col)) * buttonHeight)
+				
+				local effectDisabled = chaosEffects.disabledEffects[value] ~= nil
+				
 				if effectDisabled then
-					chaosEffects.disabledEffects[value] = nil
+					UiButtonImageBox("ui/common/box-outline-6.png", 6, 6, 1, 0, 0, 1)
 				else
-					chaosEffects.disabledEffects[value] = "disabled"
+					UiButtonImageBox("ui/common/box-outline-6.png", 6, 6, 0, 1, 0, 1)
 				end
-			end
-			
-			--UiTranslate(0, -((key - 1) * 40 + 5))
-			UiTranslate(xOffset - (key % rows) * 300, -(key / col * 40))
+				
+				if UiTextButton(chaosEffects.effects[value].name, buttonWidth, buttonHeight) then
+					if effectDisabled then
+						chaosEffects.disabledEffects[value] = nil
+					else
+						chaosEffects.disabledEffects[value] = "disabled"
+					end
+				end
+				
+				UiTranslate(buttonWidth, 0)
+				
+				if key % rows == 0 then
+					UiTranslate(0, buttonHeight)
+					UiTranslate(-buttonWidth*(rows), 0) 
+				end
+			--UiPop()
 		end
-	UiPush()
+	UiPop()
 	
 end
 
